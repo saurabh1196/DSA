4adeb115ef4cb03e62e554dc6b80ad01f45e1eac
saurabh1196 <saurabh1196@gmail.com>
2025-01-19 15:42:47 +0530

    Initial Commit

---
+++ Learncpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Learncpp
+//
+//  Created by Saurabh Singh Dangi on 19/01/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
